9028f2f3debcf50bb2667f8a9b48986ee3e90184
Jamie Buck <jbuck94@comcast.net>
2024-03-23 15:32:13 -0400

    more cleanup

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,6 +1,7 @@
 module.exports = {
-  extends: ['next', 'prettier'],
-  plugins: ['@typescript-eslint'],
+  extends: ['prettier'],
+  ignorePatterns: ['node_modules', 'dist'],
+  plugins: ['@typescript-eslint', 'unused-imports', 'prettier'],
   settings: {
     next: {
       rootDir: ['apps/*/', 'packages/*/'],
@@ -11,7 +12,17 @@ module.exports = {
     },
   },
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'unused-imports/no-unused-imports': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
   },
 };
