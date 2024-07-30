87164126557b68c07c89cea56b7ffc02700006ad
Jamie Buck <jbuck94@comcast.net>
2024-07-30 08:22:45 -0400

    add the right env vars

--- infra/api_service.tf
+++ infra/api_service.tf
@@ -1,8 +1,9 @@
 
 module "api_service" {
-  source         = "./modules/cloud_run_service"
-  service_name   = "api"
-  region         = var.region
-  image          = "gcr.io/${var.project_id}/api:latest"
+  source       = "./modules/cloud_run_service"
+  service_name = "api"
+  region       = var.region
+  # image          = "gcr.io/${var.project_id}/api:latest"
+  image          = "gcr.io/cloudrun/hello"
   container_port = 4000
 }
--- infra/web_service.tf
+++ infra/web_service.tf
@@ -1,8 +1,9 @@
 
 module "web_service" {
-  source         = "./modules/cloud_run_service"
-  service_name   = "web"
-  region         = var.region
-  image          = "gcr.io/${var.project_id}/web:latest"
+  source       = "./modules/cloud_run_service"
+  service_name = "web"
+  region       = var.region
+  # image          = "gcr.io/${var.project_id}/web:latest"
+  image          = "gcr.io/cloudrun/hello"
   container_port = 3000
 }
